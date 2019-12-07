8fc40f45a06c8c9b1c248769f8a4c427fc825e85
Johan Smet <johan.smet@justcode.be>
2019-12-07 13:35:43 +0100

    Day 6

---
+++ 06/part1.cpp
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+static const char *INPUT_FILE = "input.txt";
+
+struct node_t {
+	string				name;
+	node_t *			parent;
+	vector<node_t *>	children;
+};
+
+using map_t = unordered_map<string, node_t>;
+
+node_t *fetch_or_create_node(map_t &map, const string &name, node_t *parent) {
+	node_t *result;
+
+	auto found = map.find(name);
+	if (found != end(map)) {
+		result = &found->second;
+	} else {
+		result = &map[name];
+		result->name = name;
+		result->parent = nullptr;
+	}
+
+	if (result->parent == NULL && parent != NULL) {
+		result->parent = parent;
+		parent->children.push_back(result);
+	}
+
+	return result;
+}
+
+bool read_map(map_t &map) {
+	std::ifstream input(INPUT_FILE, std::ios::in);
+	if (!input.is_open()) {
+		std::cout << "Error opening file " << INPUT_FILE << std::endl;
+		return false;
+	}
+
+	string line;
+
+	while (input >> line) {
+		auto delim = line.find_first_of(')');
+		auto center_name = line.substr(0, delim);
+		auto object_name = line.substr(delim + 1);
+
+		auto center = fetch_or_create_node(map, center_name, NULL);
+		auto object = fetch_or_create_node(map, object_name, center);
+	}
+
+	input.close();
+	return true;
+}
+
+int checksum_map(node_t *node, int sequence) {
+	
+	int result = sequence;
+	for (auto child : node->children) {
+		result += checksum_map(child, sequence + 1);
+	}
+	
+	return result;
+}
+
+int main() {
+	map_t map;
+
+	if (!read_map(map)) {
+		return -1;
+	}
+
+	auto com_iter = map.find("COM");
+	if (com_iter == end(map)) {
+		cout << "No CenterOfMass" << endl;
+		return -1;
+	}
+	auto com = &com_iter->second;
+
+	cout << checksum_map(com, 0) << endl;
+	
+	return 0;
+}
