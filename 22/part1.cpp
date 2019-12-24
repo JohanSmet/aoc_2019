4b1e4e1879ac26da3f920db1c434869d1fef2760
Johan Smet <johan.smet@justcode.be>
2019-12-24 22:53:20 +0100

    Day 22 - part 1 (modular arithmetic)

--- 22/part1.cpp
+++ 22/part1.cpp
@@ -6,77 +6,43 @@
 using namespace std;
 
 static constexpr int DECK_SIZE = 10007;
+static constexpr int CARD = 2019;
 
-inline void dump_vector(const char *label, const vector<int> &v) {
-	cout << label;
-	for (auto i : v) {
-		cout << " " << i;
-	}
-	cout << endl;
-}
-
-vector<int> deal_with_increment(const vector<int> &deck, int inc) {
-	cout << "deal with increment = " << inc << endl;
-	
-	vector<int> result(deck.size(), -1);
-	int src = 0;
-	int dst = 0;
-
-	while (src < deck.size()) {
-		result[dst] = deck[src++];
-		dst = (dst + inc) % deck.size();
-	}
-
-	return result;
-}
-
-vector<int> deal_into_new_stack(const vector<int> &deck) {
-	cout << "deal into new stack" << endl;
-
-	vector<int> result;
-	result.reserve(deck.size());
-	reverse_copy(begin(deck), end(deck), back_inserter(result));
-	return result;
-}
-
-vector<int> cut(const vector<int> &deck, int n) {
-	cout << "cut = " << n << endl;
-
-	vector<int> result;
-	result.reserve(deck.size());
-
-	if (n > 0) {
-		copy(begin(deck)+n,end(deck), back_inserter(result));
-		copy_n(begin(deck), n, back_inserter(result));
-	} else {
-		copy(begin(deck) + deck.size() + n, end(deck), back_inserter(result));
-		copy_n(begin(deck), deck.size() + n, back_inserter(result));
-	}
-
-	return result;
+inline int mod(int a, int b) {
+	return (a % b + b) % b;
 }
 
 int main() {
-	vector<int>	deck(DECK_SIZE);
-	iota(begin(deck), end(deck), 0);
-
 	string shuffle_cmd;
+	int a = 1;
+	int b = 0;
+
+	// the shuffle commands can be written as affine transformation in modular arithmetic
+	// of the form pos(x) = (a * x + b) mod DECK_SIZE
+	//  - reverse(x) = (DECK_SIZE - x - 1) mod DECK_SIZE
+	//	- cut(x,n) = (x + DECK_SIZE - n) mod DECK_SIZE
+	//	- inc(x,n) = (x * n) mod DECK_SIZE
+	// we combine these transformations into a single transformation and apply it to the wanted card
 
 	while(getline(cin, shuffle_cmd)) {
 		if (shuffle_cmd.starts_with("deal with increment")) {
-			deck = deal_with_increment(deck, stoi(shuffle_cmd.substr(20)));
+			int n = stoi(shuffle_cmd.substr(20));
+			a = mod(n * a, DECK_SIZE);
+			b = mod(n * b, DECK_SIZE);
 		} else if (shuffle_cmd.starts_with("deal into new stack")) {
-			deck = deal_into_new_stack(deck);
+			a = mod(-a, DECK_SIZE);
+			b = mod(-b + DECK_SIZE - 1, DECK_SIZE);
 		} else if (shuffle_cmd.starts_with("cut")) {
-			deck = cut(deck, stoi(shuffle_cmd.substr(4)));
+			int n = stoi(shuffle_cmd.substr(4));
+			a = mod(a, DECK_SIZE);
+			b = mod(b + DECK_SIZE - n, DECK_SIZE);
 		} else {
 			cout << "Unknown command: " << shuffle_cmd << endl;
 		}
+
 	}
 
-	auto found = find(begin(deck), end(deck), 2019);
-	cout << "Card 2019 is at position " << found - begin(deck) << endl;
-
+	cout << "Card " << CARD << " ends up at position " << mod(a * CARD + b, DECK_SIZE) << endl;
 
 	return 0;
 }
