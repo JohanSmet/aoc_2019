d5e0531ca4bc4d0df287a4eee4cc16baeb7dd44f
Johan Smet <johan.smet@justcode.be>
2019-12-28 16:52:19 +0100

    Day 8: use iostream

--- 08/part1.cpp
+++ 08/part1.cpp
@@ -1,43 +1,23 @@
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
 #include <limits>
+#include <vector>
 
 using namespace std;
 
-static const char *INPUT_FILE = "input.txt";
 static constexpr int IMG_WIDTH  = 25;
 static constexpr int IMG_HEIGHT = 6;
 static constexpr int IMG_LAYER_SIZE = IMG_WIDTH * IMG_HEIGHT;
-
-void dump_vector(const char *label, vector<char>::const_iterator b, vector<char>::const_iterator e) {
-
-	cout << label;
-	for (auto i = b; i != e; ++i) {
-		cout << " " << *i;
-	}
-	cout << endl;
-}
 
 int main() {
 	vector<char> image;
 
 	// read source
-	std::string source;
-	ifstream is(INPUT_FILE, ios::in);
-	if (!is.is_open()) {
-		cerr << "Error opening input" << endl;
-		return -1;
-	}
 	char c;
-	while (is >> c) {
+	while (cin >> c) {
 		image.push_back(c);
 	}
-
-	is.close();
 
 	// iterate through the layers
 	auto layer_begin = begin(image);
