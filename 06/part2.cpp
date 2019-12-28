09ca0af32cafc599ec4c431c3e5df1d066d4b417
Johan Smet <johan.smet@justcode.be>
2019-12-28 16:24:43 +0100

    Day 6: refactor map reading

--- 06/part2.cpp
+++ 06/part2.cpp
@@ -6,34 +6,12 @@
 using namespace std;
 
 struct node_t {
-	string				name;
-	node_t *			parent;
+	node_t *			parent = nullptr;
 	vector<node_t *>	children;
-	bool				visited;
+	bool				visited = false;
 };
 
 using map_t = unordered_map<string, node_t>;
-
-node_t *fetch_or_create_node(map_t &map, const string &name, node_t *parent) {
-	node_t *result;
-
-	auto found = map.find(name);
-	if (found != end(map)) {
-		result = &found->second;
-	} else {
-		result = &map[name];
-		result->name = name;
-		result->parent = nullptr;
-		result->visited = false;
-	}
-
-	if (result->parent == NULL && parent != NULL) {
-		result->parent = parent;
-		parent->children.push_back(result);
-	}
-
-	return result;
-}
 
 node_t *fetch_node(map_t &map, const string &name) {
 	auto found = map.find(name);
@@ -52,8 +30,12 @@ bool read_map(map_t &map) {
 		auto center_name = line.substr(0, delim);
 		auto object_name = line.substr(delim + 1);
 
-		auto center = fetch_or_create_node(map, center_name, NULL);
-		auto object = fetch_or_create_node(map, object_name, center);
+		auto center = &map[center_name];
+		auto object = &map[object_name];
+		if (object->parent == nullptr) {
+			object->parent = center;
+			center->children.push_back(object);
+		}
 	}
 
 	return true;
