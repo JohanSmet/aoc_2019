9ba5c22290d142899164035d7f009b5d3a139cf3
Johan Smet <johan.smet@justcode.be>
2019-12-29 23:23:30 +0100

    Day 21: change loading of program source

--- 21/part2.cpp
+++ 21/part2.cpp
@@ -1,10 +1,7 @@
-#include <sstream>
-#include <iostream>
-#include <array>
-#include <vector>
 #include <cassert>
 #include <functional>
-#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -30,18 +27,15 @@ public:
 	bool			error = false;
 };
 
-
-program_t parse_program_source(const string &src) {
-
-	stringstream ss(src);
+program_t read_program_source() {
 
 	program_t result;
 	base_t v;
-	char c;
+	char delim;
 	
-	while (ss >> v) {
+	while (cin >> v) {
 		result.push_back(v);
-		ss >> c;
+		cin >> delim;
 	}
 
 	return result;
@@ -158,12 +152,8 @@ void Computer::run(input_callback_t input_func, output_callback_t output_func) {
 }
 
 int main() {
-	// read source
-	std::string source;
-	cin >> source;
-
 	// parse program
-	auto program = parse_program_source(source); 
+	auto program = read_program_source(); 
 
 	// run - springscript deduced manually by running the program lots of times
 	string input = 
